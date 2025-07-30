82f0dcc3760f029baa277c47a229563e2ca1f52a
smrenato <renatosmx@gmail.com>
2025-07-29 21:33:08 -0300

    hauuhaua super incrivel

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,40 +1,13 @@
--- Enable lsp
-vim.lsp.enable({ 'lua-ls' })
-
 -- Configuring some options
 vim.diagnostic.config({
 
   virtual_line = {
     current_line = true,
   },
-  severity_sort = true,
-  float = { border = 'rounded', source = 'if_many' },
-  underline = { severity = vim.diagnostic.severity.ERROR },
-  signs = {
-    text = {
-      [vim.diagnostic.severity.ERROR] = '󰅚 ',
-      [vim.diagnostic.severity.WARN] = '󰀪 ',
-      [vim.diagnostic.severity.INFO] = '󰋽 ',
-      [vim.diagnostic.severity.HINT] = '󰌶 ',
-    },
-  },
-  virtual_text = {
-    source = 'if_many',
-    spacing = 2,
-    format = function(diagnostic)
-      local diagnostic_message = {
-        [vim.diagnostic.severity.ERROR] = diagnostic.message,
-        [vim.diagnostic.severity.WARN] = diagnostic.message,
-        [vim.diagnostic.severity.INFO] = diagnostic.message,
-        [vim.diagnostic.severity.HINT] = diagnostic.message,
-      }
-      return diagnostic_message[diagnostic.severity]
-    end,
-  },
 })
 
 -- Setup language servers.
--- Detup blink-cmp
+-- Setup blink-cmp
 local blink = function(capabilities)
   return require('blink.cmp').get_lsp_capabilities(capabilities)
 end
@@ -57,11 +30,14 @@ vim.api.nvim_create_autocmd('LspAttach', {
     -- Unset 'formatexpr'
     vim.bo[args.buf].formatexpr = nil
     -- Unset 'omnifunc'
-    vim.bo[args.buf].omnifujc = nil
+    vim.bo[args.buf].omnifunc = nil
     -- Unmap K
-    vim.keymap.del('n', 'K', { buffer = args.buf })
+    -- vim.keymap.del('n', 'K', { buffer = args.buf })
 
     -- Disable document colors
-    vim.lsp.buf.document_color.enable(false, args.buf)
+    -- vim.lsp.buf.document_color.enable(false, args.buf)
   end,
 })
+
+-- Enable lsp
+vim.lsp.enable({ 'lua_ls', 'pylsp', 'ruff', 'rust_analyzer' })
